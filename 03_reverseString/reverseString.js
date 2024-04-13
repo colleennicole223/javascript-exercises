4fd17284201ed46cabcb97bfd037611c96aaca9d
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-13 10:27:28 -0900

    Finished Lesson 3, passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    let reversed = ""
+    while (sentence.length>0){
+        reversed += sentence.slice(sentence.length-1);
+        sentence = sentence.slice(0,sentence.length-1)
+    } return reversed
 
 };
 
