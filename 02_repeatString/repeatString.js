40d877c399fd40646c11c091ea2b56bb69e3aa08
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-13 10:10:42 -0900

    Completed Lesson 1 and 2, passed all Tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+
+const repeatString = function(inputWord, inputNumber) {
+    let repeatedWord = ""
+
+    if (inputNumber<0){
+        return "ERROR"
+    } else {
+        for (i=1; i<(inputNumber+1); i++){
+            repeatedWord += inputWord;
+        }
+        return repeatedWord
+    }
 };
 
 // Do not edit below this line
