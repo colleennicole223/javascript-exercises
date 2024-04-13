d542be1a3070b813ad8fd59da313935277c9ff6b
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-13 11:07:30 -0900

    Lesson 4 complete, test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems) {
+    let newarray = [];
 
-};
+    array.forEach((item) => {
+        if(!removeItems.includes(item)){
+            newarray.push(item);
+        }
+    }); 
+    return newarray;
+} 
 
 // Do not edit below this line
 module.exports = removeFromArray;
