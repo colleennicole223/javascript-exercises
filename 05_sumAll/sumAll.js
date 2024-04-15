294477ac7dd22c366215630c44f119b29b440fbe
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-15 09:30:54 -0900

    Finished Lesson 5, passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numberInput1, numberInput2) {
+    let startingNumber = 0;
+    let endingNumber = 0;
+    let sumNumbers = 0;
 
+    if (numberInput1 > numberInput2){
+        startingNumber = numberInput2;
+        endingNumber = numberInput1;
+    } else if (numberInput1<0 || numberInput2<0){
+        return "ERROR"
+    } else if (!Number.isInteger(numberInput1) || !Number.isInteger(numberInput2)){
+        return "ERROR"
+    } else {
+        startingNumber = numberInput1;
+        endingNumber = numberInput2;
+    }
+    for(i=startingNumber; i<endingNumber+1; i++){
+        sumNumbers += i;
+    } return sumNumbers
 };
 
 // Do not edit below this line
