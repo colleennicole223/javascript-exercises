9583cbc20c1cfabe0176686ef434336ca67a1b3d
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-15 13:33:23 -0900

    Finished Lesson 7, all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  if (!celsius%1==0){
+    return parseFloat(celsius.toFixed(1));
+  }
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 1.8) + 32;
+  if (!fahrenheit%1==0){
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  return fahrenheit
 };
 
 // Do not edit below this line
