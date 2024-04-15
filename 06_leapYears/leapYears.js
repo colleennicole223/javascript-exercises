4f5e9dd80e9f7fb2945d64a3c87ba50bae9538be
Colleen <153748501+colleennicole223@users.noreply.github.com>
2024-04-15 09:53:12 -0900

    Finished lesson 6, passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+/*
+- check to see if divisable by four
+- if they are, skip ones divisable by 100 unless dividable by 400
+- return true or false 
+*/
+
+const leapYears = function(year) {
+
+if(year%4 == 0){
+    if(year%100==0 && year%400!=0){
+        return false
+    }
+    return true
+} else{
+    return false
+}
 
 };
 
