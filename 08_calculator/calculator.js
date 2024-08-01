49e6ffd91e30f2bff272e2f925b91fb831d95d20
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-08-01 12:23:36 -0400

    Completed Lesson 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce(((total,current) => total + current),0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((product,current) => product * current));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b); 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  // a*(a-1)*(a-2)(a-3)*...etc
+  if(a == 0 || a == 1){
+    return 1;
+  }else{
+    let b = a;
+    for(i=1; i<a; i++){
+      b *= (a-i);
+    }
+    return b;
+  }
 };
 
 // Do not edit below this line
