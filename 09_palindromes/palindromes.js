8fb5f4d556a2075ed2eb34900f2976695afdfc54
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-08-02 13:23:08 -0400

    Completed lesson 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
 
+
+
+const palindromes = function (string) {
+
+    // Only what letters and numbers 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    updatedString = string.toLowerCase().split("").filter((character)=>alphanumerical.includes(character)).join("")
+    reversedString = updatedString.split("").reverse().join("");
+    if(updatedString===reversedString){
+        return true; 
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
