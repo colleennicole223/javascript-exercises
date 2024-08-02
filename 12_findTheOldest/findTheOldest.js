2a7f54793927553b24fdeae0356d70c9083ca519
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-08-02 16:56:05 -0400

    Completed Lesson 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestName, currentName)=>{
+        let oldestAge = getAge(oldestName.yearOfBirth, oldestName.yearOfDeath);
+        let currentAge = getAge(currentName.yearOfBirth, currentName.yearOfDeath);
+        return oldestAge < currentAge ? currentName : oldestName;
+    });
+        
 };
+
+function getAge(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
