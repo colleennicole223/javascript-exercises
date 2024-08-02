f6150b0d8908b31105e8dcce2058437f6f3c169e
colleennicole223 <153748501+colleennicole223@users.noreply.github.com>
2024-08-02 16:16:42 -0400

    Completed lesson 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+// 1[1] 1[2] 2[3] 3[4 num] 5[5] 8[6]
+    if(number == 0){return 0;}
+    if(number < 0){return "OOPS";}
 
+    let twoAgo = 1;
+    let oneAgo = 1; 
+
+    for (let i = 2; i < number; i++){
+        let current = oneAgo + twoAgo;
+        twoAgo = oneAgo;
+        oneAgo = current;
+    }
+    return oneAgo;
 };
 
 // Do not edit below this line
